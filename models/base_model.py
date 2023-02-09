ae20193db0c7ff53d18765438bb7fd17a64c1818
Atemmuda <yaw.ayeboahfo@gmail.com>
2023-02-09 15:40:51 +0000

    init and str functions added

--- models/base_model.py
+++ models/base_model.py
@@ -4,7 +4,22 @@ Base model for object serialization, deserialization,
 seve to file, retrieving from file, whic happens as used by other models
 """
 
+import uuid
+from datetime import datetime
+
 
 class BaseModel():
     """Base model class for the project"""
-    pass
+    id = None
+    created_at = None
+    updated_at = None
+
+    def __init__(self) -> None:
+        """constructor for the object"""
+        self.id = uuid.uuid4()
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        return "[{}] ({}) {}".format(type(self).__name__,
+                                     self.id, self.__dict__)
--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -3,6 +3,8 @@
 """Model for testing the BaseModel class
 """
 import unittest
+import uuid
+from datetime import datetime
 from models.base_model import BaseModel
 
 
@@ -10,5 +12,17 @@ class TestBaseModel(unittest.TestCase):
     """For testing most/all possible scenarios"""
 
     def test_class_documentation(self):
+        """testing if class is documented"""
+        self.assertGreater(len(BaseModel.__doc__), 0)
+
+    def test_init_documentation(self):
+        """testing if class is documented"""
+        self.assertGreater(len(BaseModel.__init__.__doc__), 0)
+
+    def test_initialization(self):
+        """testing for the object initialization"""
+        self.assertEqual(print(BaseModel), None)
+
+    def test_str_documentation(self):
         """testing if class is documented"""
         self.assertGreater(len(BaseModel.__doc__), 0)
